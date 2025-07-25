a059bc17c9f8f4ab56df865e961164fc71f6ed03
Pushpesh Pandey <pushpesh.kvm@gmail.com>
2025-07-25 18:38:00 +0530

    save log file on created bucket on gcp

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, request
 from google.cloud import firestore
 import os
+from datetime import datetime
+
 
 app = Flask(__name__)
-db = firestore.Client()
+def upload_to_bucket(data):
+    bucket_name = "gcp_pushpesh"
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
 
-@app.route('/')
-def form():
-    return render_template('form.html')
+    # Unique file name with timestamp
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = f"contact_submissions/form_{now}.txt"
+    blob = bucket.blob(file_name)
+
+    content = f"Name: {data['name']}\nEmail: {data['email']}\nMessage: {data['message']}\n"
+    blob.upload_from_string(content)
+    print(f"Uploaded to bucket: {file_name}")
+
+db = firestore.Client()
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -16,9 +28,8 @@ def submit():
         "email": request.form['email'],
         "message": request.form['message']
     }
-    db.collection('contacts').add(data)
-    return "Thanks! Your message has been received."
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  #Cloud Run uses env PORT
-    app.run(host='0.0.0.0', port=port, debug=False)
+    db.collection('contacts').add(data)     # 🔹 Firestore में store
+    upload_to_bucket(data)                  # 🔹 Cloud Storage में .txt file भेजो
+
+    return "Thanks! Your message has been received."
